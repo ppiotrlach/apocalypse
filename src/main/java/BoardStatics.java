eecc976441180b4bce1da9ec236399141834074b
Piotr Łach <lachpio999@gmail.com>
2020-04-23 22:32:12 +0200

    added diagrams

--- src/main/java/BoardStatics.java
+++ src/main/java/BoardStatics.java
@@ -1,8 +1,8 @@
 public abstract class BoardStatics {
 
 
-    static final int HEIGHT = 10;
-    static final int WIDTH = 10;
+    public static final int HEIGHT = 10;
+    public static final int WIDTH = 10;
 
 
 }
